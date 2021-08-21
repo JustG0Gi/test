5e0eb4d82ebf091040bdb0c4497ee1ae14a2368c
JustG0Gi <79056205+JustG0Gi@users.noreply.github.com>
2021-08-21 14:52:14 +0300

    update script.js

--- js/script.js
+++ js/script.js
@@ -1,42 +1,42 @@
 function countdowm(dateEnd) {
-	var timer, days, hours, minutes, seconds;
-	dateEnd = new Date(dateEnd);
-	dateEnd = dateEnd.getTime();
-	if (isNaN(dateEnd)) {
-		return;
-	}
+    var timer, days, hours, minutes, seconds;
+    dateEnd = new Date(dateEnd);
+    dateEnd = dateEnd.getTime();
+    if (isNaN(dateEnd)) {
+        return;
+    }
 
-	timer = setInterval(calculate, 1000);
+    timer = setInterval(calculate, 1000);
 
-	function calculate() {
-		var dateStart = new Date();
-		var dateStart = new Date(dateStart.getFullYear(),
-			dateStart.getUTCMonth(),
-			dateStart.getUTCDate(),
-			dateStart.getUTCHours(),
-			dateStart.getUTCMinutes(),
-			dateStart.getUTCSeconds());
-		var timeRemaining = parseInt((dateEnd - dateStart.getTime()) / 1000)
+    function calculate() {
+        var dateStart = new Date();
+        var dateStart = new Date(dateStart.getFullYear(),
+            dateStart.getUTCMonth(),
+            dateStart.getUTCDate(),
+            dateStart.getUTCHours(),
+            dateStart.getUTCMinutes(),
+            dateStart.getUTCSeconds());
+        var timeRemaining = parseInt((dateEnd - dateStart.getTime()) / 1000)
 
-		if (timeRemaining >= 0) {
-			days = parseInt(timeRemaining / 86400);
-			timeRemaining = (timeRemaining % 86400);
-			hours = parseInt(timeRemaining / 3600);
-			timeRemaining = (timeRemaining % 3600);
-			minutes = parseInt(timeRemaining / 60);
-			timeRemaining = (timeRemaining % 60);
-			seconds = parseInt(timeRemaining);
+        if (timeRemaining >= 0) {
+            days = parseInt(timeRemaining / 86400);
+            timeRemaining = (timeRemaining % 86400);
+            hours = parseInt(timeRemaining / 3600);
+            timeRemaining = (timeRemaining % 3600);
+            minutes = parseInt(timeRemaining / 60);
+            timeRemaining = (timeRemaining % 60);
+            seconds = parseInt(timeRemaining);
 
-			document.getElementById("days").innerHTML = parseInt(days, 6);
-			document.getElementById("hours").innerHTML = ("0" + hours).slice(-2);
-			document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2);
-			document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
-		} else {
-			return;
-		}
-	}
+            document.getElementById("days").innerHTML = parseInt(days, 6);
+            document.getElementById("hours").innerHTML = ("0" + hours).slice(-2);
+            document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2);
+            document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
+        } else {
+            return;
+        }
+    }
 
-	function display(days, hours, minutes, seconds) { }
+    function display(days, hours, minutes, seconds) {}
 }
 
-countdowm('08/18/2021 02:00:00 AM');
+countdowm('12/29/2021 02:00:00 AM');
